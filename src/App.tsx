f76aa72eafcd41b2af3e49ffefa983f705ec63ff
danya_vuejs <da.andruk@insspring.com>
2022-09-02 16:59:54 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMutation, gql } from "@apollo/client";
+import { MouseEvent } from "react";
 
-function App() {
+const LOGIN = gql`
+  mutation UserLogin($email: String!, $password: String! ) {
+    login(email: $email, password: $password) {
+      ttl
+      secret
+      email
+    }
+  }
+`;
+
+export default function Home() {
+
+  const [loginFunc, { loading, error }] = useMutation(LOGIN)
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  const doLogin = (e: MouseEvent) => {
+    e.preventDefault();
+    loginFunc({
+        variables: {
+          email: 'danya@gmail.com',
+          password: 'Pass1234',
+        }
+    })
+    .then(resp => console.log('==>', resp))
+    .catch(e => console.log(e))   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={doLogin}>Login</button>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/apollo-client.js
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://graphql.eu.fauna.com/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.REACT_APP_PUBLIC_FAUNA_SECRET
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+export default client;
